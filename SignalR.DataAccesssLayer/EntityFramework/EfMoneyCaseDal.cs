a68b71aa99f3c40ab95de80d3eece6e063f91db4
AybukeGrr <ce.aybukegurer@gmail.com>
2024-04-18 23:42:25 +0300

    instant cash transactions started

---
+++ SignalR.DataAccesssLayer/EntityFramework/EfMoneyCaseDal.cs
@@ -0,0 +1,24 @@
+﻿using SignalR.DataAccesssLayer.Abstract;
+using SignalR.DataAccesssLayer.Concrete;
+using SignalR.DataAccesssLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccesssLayer.EntityFramework
+{
+    public class EfMoneyCaseDal : GenericRepository<MoneyCase>, IMoneyCaseDal
+    {
+        public EfMoneyCaseDal(SignalRContext context) : base(context)
+        {
+        }
+
+        //public decimal TotalMoneyCaseAmount()
+        //{
+        //    using var context = new SignalRContext();
+        //}
+    }
+}

61cb8ea2a4d3b69bb406becada5cd3f767aa00be
AybukeGrr <ce.aybukegurer@gmail.com>
2024-05-23 18:45:16 +0300

    added basket class

---
+++ SignalR.DataAccesssLayer/EntityFramework/EfBasketDal.cs
@@ -0,0 +1,26 @@
+﻿using SignalR.DataAccesssLayer.Abstract;
+using SignalR.DataAccesssLayer.Concrete;
+using SignalR.DataAccesssLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccesssLayer.EntityFramework
+{
+    public class EfBasketDal : GenericRepository<Basket>, IBasketDal
+    {
+        public EfBasketDal(SignalRContext context) : base(context)
+        {
+        }
+
+        public List<Basket> GetBasketByMenuTableNumber(int id)
+        {
+            using var context = new SignalRContext();
+            var values = context.Baskets.Where(x=>x.MenuTableID == id).ToList();
+            return values;
+        }
+    }
+}
--- SignalR.EntityLayer/Entities/MenuTable.cs
+++ SignalR.EntityLayer/Entities/MenuTable.cs
@@ -13,5 +13,6 @@ namespace SignalR.EntityLayer.Entities
         public int MenuTableID { get; set; }
         public string Name { get; set; }
         public bool Status { get; set; }
+        public List<Basket> Baskets { get; set; }
     }
 }
